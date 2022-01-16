a2f8e2e112593cba25ca1237afbe9224d7d3e324
Galva <alvarojuan2014@gmail.com>
2022-01-16 18:19:21 -0400

    Envio de mensajes mediante php en la pagina web del ejercicio 1

--- ejercicio1/contact.php
+++ ejercicio1/contact.php
@@ -1,28 +1,51 @@
 <!-- Contact -->
 <section id="contact" class="four">
-						<div class="container">
+    <div class="container">
 
-							<header>
-								<h2>Contact</h2>
-							</header>
+        <header>
+            <h2>Contact</h2>
+        </header>
 
-							<p>Elementum sem parturient nulla quam placerat viverra
-							mauris non cum elit tempus ullamcorper dolor. Libero rutrum ut lacinia
-							donec curae mus. Eleifend id porttitor ac ultricies lobortis sem nunc
-							orci ridiculus faucibus a consectetur. Porttitor curae mauris urna mi dolor.</p>
+        <p>Elementum sem parturient nulla quam placerat viverra
+            mauris non cum elit tempus ullamcorper dolor. Libero rutrum ut lacinia
+            donec curae mus. Eleifend id porttitor ac ultricies lobortis sem nunc
+            orci ridiculus faucibus a consectetur. Porttitor curae mauris urna mi dolor.</p>
 
-							<form method="post" action="#">
-								<div class="row">
-									<div class="col-6 col-12-mobile"><input type="text" name="name" placeholder="Name" /></div>
-									<div class="col-6 col-12-mobile"><input type="text" name="email" placeholder="Email" /></div>
-									<div class="col-12">
-										<textarea name="message" placeholder="Message"></textarea>
-									</div>
-									<div class="col-12">
-										<input type="submit" value="Send Message" />
-									</div>
-								</div>
-							</form>
+        <form method="post" action="#">
+            <div class="row">
+                <div class="col-6 col-12-mobile">
+                    <input type="text" name="name" placeholder="Name" id="txtNombre" />
+                </div>
+                <div class="col-6 col-12-mobile">
+                    <input type="text" name="email" placeholder="Email" id="txtEmail" />
+                </div>
+                <div class="col-12">
+                    <textarea name="message" placeholder="Message" id="txtMensaje"></textarea>
+                </div>
+                <div class="col-12">
+                    <button type="button" id="btnSubmit">Send</button>
+                </div>
+            </div>
+        </form>
 
-						</div>
-					</section>
+    </div>
+</section>
+
+<script>
+let btnSubmit = window.document.getElementById("btnSubmit");
+btnSubmit.addEventListener('click', () => {
+    let nombre = window.document.getElementById("txtNombre").value;
+    let email = window.document.getElementById("txtEmail").value;
+    let mensaje = window.document.getElementById("txtMensaje").value;
+
+    fetch('email.php?nombre='+nombre+'&email='+email+'&mensaje='+mensaje, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then(e => {
+		// alert(e);
+	});
+});
+</script>
--- ejercicio1/intro.php
+++ ejercicio1/intro.php
@@ -1,17 +1,18 @@
 <!-- Intro -->
 <section id="top" class="one dark cover">
-						<div class="container">
+    <div class="container">
 
-							<header>
-								<h2 class="alt">Hi! I'm <strong>Prologue</strong>, a <a href="http://html5up.net/license">free</a> responsive<br />
-								site template designed by <a href="http://html5up.net">HTML5 UP</a>.</h2>
-								<p>Ligula scelerisque justo sem accumsan diam quis<br />
-								vitae natoque dictum sollicitudin elementum.</p>
-							</header>
+        <header>
+            <h2 class="alt">Hi! I'm <strong>Prologue</strong>, a <a href="http://html5up.net/license">free</a>
+                responsive<br />
+                site template designed by <a href="http://html5up.net">HTML5 UP</a>.</h2>
+            <p>Ligula scelerisque justo sem accumsan diam quis<br />
+                vitae natoque dictum sollicitudin elementum.</p>
+        </header>
 
-							<footer>
-								<a href="#portfolio" class="button scrolly">Magna Aliquam</a>
-							</footer>
+        <footer>
+            <a href="#portfolio" class="button scrolly">Magna Aliquam</a>
+        </footer>
 
-						</div>
-					</section>
+    </div>
+</section>
